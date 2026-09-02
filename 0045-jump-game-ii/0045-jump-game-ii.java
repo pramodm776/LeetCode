7671f6dba2da99b7e1f62967dc4cd43747b43055
pramodm776 <pramodmadabhavi1418@gmail.com>
2026-09-02 20:03:55 +0530

    Time: 1 ms (99.79%), Space: 46.8 MB (96.93%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+
+        int far = 0;
+        int jump=0;
+        int end =0;
+
+        for(int i=0; i<nums.length-1;i++){
+
+
+            far = Math.max(far,i+nums[i]);
+
+            if(i==end){
+                jump++;
+
+                end = far;
+            }
+        }
+
+        return jump;
+        
+    }
+}
